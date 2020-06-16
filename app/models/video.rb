b0a8e7cfa19f31ca8547e3b73ab83b155fce90be
jinnic <yoohyojin@gmail.com>
2020-06-15 20:27:03 -0400

    last updated at msg

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,13 @@ class Video < ApplicationRecord
     # byebug
     src = VideoEmbedUrlGenerator.new(src)
   end
+
+  def time_diffrence(updated_at)
+    now = Time.now
+    if self.updated_at == nil
+      TimeDifference.between(now, self.created_at.time).humanize
+    else
+      TimeDifference.between(now, self.updated_at.time).humanize
+    end
+  end
 end
