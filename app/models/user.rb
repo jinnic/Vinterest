7d96957ac8c31a3158297324bad62704ae83cacb
jinnic <yoohyojin@gmail.com>
2020-09-16 19:30:27 -0400

    user board count'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,16 @@ class User < ApplicationRecord
   has_many :boards, dependent: :destroy 
 
   def public_board_count(user_id)
-    # count = Board.where(user_id: user_id).where("public == true").count
-    if Board.where(user_id: user_id).size != 0
-      Board.where(user_id: user_id).where("public == true").count
-    else
-      0
-    end
+    Board.where(user_id: user_id).where(public: true).count
   end
 
   def private_board_count(user_id)
-    # Board.where(user_id: user_id).where("public == false").count
-    if Board.where(user_id: user_id).size != 0
-      Board.where(user_id: user_id).where("public == false").count
-    else
-      # byebug
-      0
-    end
+    Board.where(user_id: user_id).where(public: false).count
+    # if Board.where(user_id: user_id).size != 0
+    #   Board.where(user_id: user_id).where(public: false).count
+    # else
+    #   0
+    # end
   end
 
 end
