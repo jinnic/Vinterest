a641bf9371d8a78a3524a82fee25300abe7b0e32
jinnic <yoohyojin@gmail.com>
2023-12-21 07:52:29 +0900

    add vercel

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -4,4 +4,9 @@ class VideoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should not have video without title" do 
+    video = Video.new
+    assert_not video.save "Saved the video without a title"
+  end 
 end
